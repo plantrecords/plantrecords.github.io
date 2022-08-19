8ce341f4109b7fd24d9dabf666311e6bf0fbdaee
plantrecords <60455566+plantrecords@users.noreply.github.com>
2022-08-19 13:43:30 -1000

    Update dashtest.php

--- dashtest.php
+++ dashtest.php
@@ -1,5 +1,5 @@
-    <iframe
-    src="http://ntbg.org:3000/public/question/2cda09e9-42bd-4d66-881b-90976901f056"
+<iframe
+    src="http://ntbg.org:3000/public/dashboard/933bf116-5b51-43ff-910a-ffd0f93f320a#theme=night"
     frameborder="0"
     width="800"
     height="600"
