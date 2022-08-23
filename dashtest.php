9a93d854e0467740d0e6ec98a96c350638dcd86f
plantrecords <60455566+plantrecords@users.noreply.github.com>
2022-08-22 16:38:50 -1000

    Update dashtest.php

--- dashtest.php
+++ dashtest.php
@@ -1,8 +1,8 @@
 <iframe
     src="http://ntbg.org:3000/public/dashboard/933bf116-5b51-43ff-910a-ffd0f93f320a#theme=night"
     frameborder="0"
-    width="1200"
-    height="900"
+    width="1920"
+    height="1080"
     allowtransparency
 ></iframe>
 <iframe width="791" height="445" src="https://www.youtube.com/embed/B3Aw4ryy3g8" title="Improving Biodiversity with GNSS at the National Tropical Botanical Garden in Hawaii" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
